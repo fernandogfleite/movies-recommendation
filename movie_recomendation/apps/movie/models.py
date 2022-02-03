3560223d4279d1f92fe0a1db147d05da39078293
Fernando Gabriel <fernandogfleite@gmail.com>
2022-02-03 18:03:35 -0300

    Mudança para salvar os gêneros dos filmes em uma tabela de relacionamento em vez de usar ManyToManyField. Criação de um model para salvar as avaliações dos filmes

--- movie_recomendation/apps/movie/models.py
+++ movie_recomendation/apps/movie/models.py
@@ -15,7 +15,6 @@ class Genre(models.Model):
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
-    genres = models.ManyToManyField(Genre)
     tmdb = models.TextField(blank=True, null=True)
     imdb = models.TextField(blank=True, null=True)
 
@@ -24,3 +23,24 @@ class Movie(models.Model):
 
     class Meta:
         ordering = ('title',)
+
+
+class MovieGenre(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.movie.title} - {self.genre.name}'
+
+
+class MovieRating(models.Model):
+    user_id = models.IntegerField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.FloatField()
+    timestamp = models.DateTimeField()
+
+    class Meta:
+        ordering = ('movie', 'rating')
+
+    def __str__(self) -> str:
+        return f'{self.movie.title} - Rating: {self.rating}'
